59a2e6347d51d94546a6138e41fd117462c2fc52
Personne17 <44241269+Personne17@users.noreply.github.com>
2018-11-18 19:15:03 +0100

    Add files via upload

---
+++ commands/si.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+module.exports.run = (client, message, args, guild) => {
+
+    let msg = message;
+
+    var si_embed = new Discord.RichEmbed()
+            .setColor("BLUE")
+            .setTitle("Informations du serveur")
+            .setDescription("Tout ce qu'il y a à savoir sur le serveur.")
+            .addField("Nom du serveur :", guild.name, true)
+            .addField("Serveur créé le :", guild.createdAt, true)
+            .addField("Nombre de salons :", msg.guild.channels.size, true)
+            .addField("Nombre de membres sur le serveur :", guild.memberCount, true)
+            .addField("Id du fondateur :", guild.ownerID, true)
+            .addField("Vous avez rejoint ce serveur le :", msg.author.guild.joinedAt, true)
+            .setFooter("Bot créé par _Personn_#0524.")
+
+        msg.channel.send(si_embed)
+}
+module.exports.help = {
+  name: "si"
+}
