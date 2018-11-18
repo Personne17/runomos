4c437d7cdda1596496d24443f268ea8038c36683
Personne17 <44241269+Personne17@users.noreply.github.com>
2018-11-18 13:39:47 +0100

    Add files via upload

--- commands/salut.js
+++ commands/salut.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = (client, msg, args, guild) => {
     var bchannel = client.channels.get("513387249153474561");
-    if(msg.channel == bchannel') {
+    if(msg.channel == bchannel) {
 
     bchannel.send(`Hey ! Salut, \`${msg.author.username}\``)
   }
