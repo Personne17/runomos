43b766efae723cb6b00e4ad8d56f5ab114167410
Personne17 <44241269+Personne17@users.noreply.github.com>
2018-11-18 19:48:43 +0100

    Update verif.js

--- commands/verif.js
+++ commands/verif.js
@@ -1,14 +1,7 @@
 
 const Discord = require("discord.js"); //acces a la librairie discord
 
-function verif() {
-  const verifchannel = client.channels.find(ch => ch.name == "verification"); //verif
-  let verifmember = msg.member;
 
-  verifmember.addRole('513346079287410689'); //ajout du role au membre
-
-  verifchannel.send(`Bienvenue sur le serveur, \`${msg.author.username}\``); //envoi du message de conirmation
-}
 module.exports.run = (client, msg, args, guild) => {
 
   if(guild.me.hasPermission("MANAGE_CHANNELS")) { // Si le bot a la permission de supprimer les messages
@@ -18,7 +11,16 @@ module.exports.run = (client, msg, args, guild) => {
   if(guild.me.hasPermission("MANAGE_ROLES")) {
       verif();
   }
+  function verif() {
+  const verifchannel = client.channels.find(ch => ch.name == "verification"); //verif
+  let verifmember = msg.member;
+
+  verifmember.addRole('513346079287410689'); //ajout du role au membre
+
+  verifchannel.send(`Bienvenue sur le serveur, \`${msg.author.username}\``); //envoi du message de conirmation
 }
+}
+
 module.exports.help = {
   name: "verif"
 }
