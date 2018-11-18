7ed7ba1a014274e7f39797cac837b843d9fae88d
Personne17 <44241269+Personne17@users.noreply.github.com>
2018-11-18 21:17:23 +0100

    Add files via upload

--- commands/verif.js
+++ commands/verif.js
@@ -12,14 +12,14 @@ module.exports.run = (client, msg, args, guild) => {
       verif();
   }
   function verif() {
-    const verifchannel = client.channels.find(ch => ch.name == "verification"); //verif
+    const verifchannel = client.channels.get("513420946523291658"); //verif
     let verifmember = msg.member;
 
     verifmember.addRole('513346079287410689'); //ajout du role au membre
 
     verifchannel.send(`Bienvenue sur le serveur, \`${msg.author.username}\``); //envoi du message de conirmation
   }
-  
+
 }
 module.exports.help = {
   name: "verif"
